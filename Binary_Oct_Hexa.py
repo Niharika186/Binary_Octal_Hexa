463d2a1153e46b91f0ab2de1340b174257195ad7
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-07-04 17:46:54 +0530

    Add files via upload

---
+++ Binary_Oct_Hexa.py
@@ -0,0 +1,36 @@
+'''Binary'''
+# a=20
+# print(bin(a))  # 0b10100
+# print(0b10100)  # 20
+# print(0B10100)  # 20
+
+'''Octal'''
+# b=16
+# print(oct(b)) # 0o20
+# print(0o20)  # 16
+# print(0O20)  # 16
+
+'''Hexa'''
+# c=34
+# print(hex(c))  # 0x22
+# print(0x22)  # 34
+# print(0X22)  # 34
+# print(hex(16))  # 0x10
+# print(0x10) # 16
+# print(0X10) # 16
+# print(0xA) # 10
+# print(0xB) # 11
+# print(0xC) # 12
+# print(0xD) # 13
+# print(0xE) # 14
+# print(0XF) # 15
+
+
+# a=01234
+# print(a) # SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+
+# a=123
+# print(a) # 123
+
+# a='123'
+# print(a)  # 123
